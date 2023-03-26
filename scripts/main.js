21cae32f62899f6e996fcef0d85a7d18a2ae5299
Zsolt Sz. Sztupák <mail@sztupy.hu>
2023-03-26 00:34:15 +0000

    Add support for displaying wins. Add support for multiple odd tables

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,19 @@
 const startMoney = 1000000;
 const now = Date.now(); //Date.parse('2023-03-25 00:00:00'); //Date.now();
 const currentVersion = 2;
-const activePos = 1;
+const activePos = 2;
 
-let data = [];
+let data = {};
+let scoreData = {};
 let validity = now;
 let money = 1000000;
 let oldCode = null;
-let basket = {}; basket[activePos] = [];
+let basket = {};
 let current = {};
 
 function upgradeData(version, data) {
     if (version == 1) {
-        let result = { 1: []};
+        let result = { 1: [] };
         for (let booking of data) {
             let amount = booking[0];
             let newBooking = {};
@@ -32,14 +33,15 @@ function updateMoney() {
     document.getElementById("remaining_money").innerHTML = `Ŧ${money.toLocaleString('hu-HU')}`;
 }
 
-function betSlipToString(slip) {
+function betSlipToString(version, slip) {
     let odds = 1;
 
     let result = '';
+    let winlose = 1;
     for (let key in slip) {
         let type = key[0];
         let id = key.substring(1);
-        let thread = data.find(item => item.id == id);
+        let thread = data[version].find(item => item.id == id);
 
         let name = "<strong>'" + thread.name + "'</strong>";
 
@@ -48,25 +50,34 @@ function betSlipToString(slip) {
         odds = odds * odd;
 
         if (type=='W') {
+            let wl = scoreData['W'][slip[key]][thread.id];
+            if (wl == -1) winlose = -1;
+            if (wl != 1 && winlose == 1) winlose = 0;
             if (slip[key] == 1) {
-                result += `<div>${name} megnyeri a bajnokságot: ${odd}</div>`;
+                result += `<div class="${getWinLoseClass(wl)}">${name} megnyeri a bajnokságot: ${odd}</div>`;
             } else if (slip[key] <= 4) {
-                result += `<div>${name} bekerül a legjobb ${slip[key]}-be: ${odd}</div>`;
+                result += `<div class="${getWinLoseClass(wl)}">${name} bekerül a legjobb ${slip[key]}-be: ${odd}</div>`;
             } else {
-                result += `<div>${name} bekerül a legjobb ${slip[key]}-ba: ${odd}</div>`;
+                result += `<div class="${getWinLoseClass(wl)}">${name} bekerül a legjobb ${slip[key]}-ba: ${odd}</div>`;
             }
 
         } else if (type=='A') {
+            let wl = scoreData['A'][slip[key]][thread.id];
+            if (wl == -1) winlose = -1;
+            if (wl != 1 && winlose == 1) winlose = 0;
             if (slip[key] == 0) {
-                result += `<div>${name} nyer: ${odd}</div>`;
+                result += `<div class="${getWinLoseClass(wl)}">${name} nyer: ${odd}</div>`;
             } else {
-                result += `<div>${name} nyer legalább ${slip[key]}%-al: ${odd}</div>`;
+                result += `<div class="${getWinLoseClass(wl)}">${name} nyer legalább ${slip[key]}%-al: ${odd}</div>`;
             }
         } else if (type=='B') {
+            let wl = scoreData['B'][slip[key]][thread.id];
+            if (wl == -1) winlose = -1;
+            if (wl != 1 && winlose == 1) winlose = 0;
             if (slip[key] == 0) {
-                result += `<div>${name} veszít: ${odd}</div>`;
+                result += `<div class="${getWinLoseClass(wl)}">${name} veszít: ${odd}</div>`;
             } else {
-                result += `<div>${name} veszít legalább ${slip[key]}%-al: ${odd}</div>`;
+                result += `<div class="${getWinLoseClass(wl)}">${name} veszít legalább ${slip[key]}%-al: ${odd}</div>`;
             }
         }
     }
@@ -75,7 +86,7 @@ function betSlipToString(slip) {
         result = 'Válassz tétet a jobb oldali odds-okra kattintva. ';
     }
 
-    return [odds,result];
+    return [parseFloat(odds.toFixed(2)),result,winlose];
 }
 
 function updateCurrent() {
@@ -92,7 +103,7 @@ function updateCurrent() {
         currentMoney.value = 1000;
     }
 
-    let [odds,result] = betSlipToString(current);
+    let [odds,result] = betSlipToString(activePos,current);
 
     currentList.innerHTML = result;
 
@@ -134,19 +145,22 @@ function updateBasket() {
     let content = '<div>';
     let totalBet = 0;
 
-    for (let slipData of basket[activePos]) {
-        let bet = slipData[0];
-        let slip = slipData[1];
+    for (let pos in basket) {
+        if (pos == 'old') continue;
+        for (let slipData of basket[pos]) {
+            let bet = slipData[0];
+            let slip = slipData[1];
 
-        let [odds,result] = betSlipToString(slip);
+            let [odds,result,winlose] = betSlipToString(pos, slip);
 
-        let betMoney = bet * parseFloat(odds.toFixed(2));
+            let betMoney = bet * parseFloat(odds.toFixed(2));
 
-        content += `<div class="slip"><div class="money"><strong>Feltett pénz</strong>: Ŧ${bet.toLocaleString('hu-HU')}</div><div class="slip_list">`;
-        content += result;
-        content += `</div><div><strong>Odds</strong>: ${odds.toFixed(2)}</div><div><strong>Potenciális nyeremény</strong>: Ŧ${(betMoney.toLocaleString('hu-HU'))}</div></div>`;
+            content += `<div class="slip ${getWinLoseClass(winlose)}"><div class="money"><strong>Feltett pénz</strong>: Ŧ${bet.toLocaleString('hu-HU')}</div><div class="slip_list">`;
+            content += result;
+            content += `</div><div><strong>Odds</strong>: ${odds.toFixed(2)}</div><div><strong>Potenciális nyeremény</strong>: Ŧ${(betMoney.toLocaleString('hu-HU'))}</div></div>`;
 
-        totalBet += bet;
+            totalBet += bet;
+        }
     }
 
     content += '</div>';
@@ -174,10 +188,17 @@ function updateBasket() {
         ctx.fillText(oldCode, 10, 180);
     }
 
+    let countBet = 0;
+    for (let key in basket) {
+        if (key!='old') {
+            countBet += basket[key].length;
+        }
+    }
+
     ctx.font = "14px sans-serif";
     ctx.fillText(`Hunbliga fogadási bizonylat ${currentVersion}.0`, 10, 50);
     ctx.fillText(`Érvényes: ${new Date(validity).toLocaleString('hu-HU', { timeZone: 'Europe/Budapest' } )}`, 10, 70);
-    ctx.fillText(`Fogadások száma: ${basket[1].length}`, 10, 90);
+    ctx.fillText(`Fogadások száma: ${countBet}`, 10, 90);
     ctx.fillText(`Feltett összeg: Ŧ${(totalBet.toLocaleString('hu-HU'))}`, 10, 110);
     ctx.fillText(`Fennmaradt pénz: Ŧ${(money.toLocaleString('hu-HU'))}`, 10, 130);
 
@@ -224,7 +245,16 @@ function resetCurrent() {
     for (let b in basket) {
         if (b!='old') {
             for (let item of basket[b]) {
+                let bet = item[0];
+                let slip = item[1];
+
+                let [odds,result,winlose] = betSlipToString(b, slip);
+
                 money -= item[0];
+
+                if (winlose == 1) {
+                    money += item[0] * odds;
+                }
             }
         }
     }
@@ -254,7 +284,12 @@ document.getElementById("current_bet").onclick = function() {
         currentMoney.value = 1000;
     }
 
-    basket[1].push([parseInt(currentMoney.value), current]);
+    let value = parseInt(currentMoney.value);
+
+    if (value <= money) {
+        basket[activePos] ||= [];
+        basket[activePos].push([parseInt(currentMoney.value), current]);
+    }
 
     resetCurrent();
 }
@@ -332,44 +367,20 @@ function loadFailed() {
     console.error("The provided file couldn't be loaded as an Image media");
 }
 
-Papa.parse('standings/1.csv', {
-    download: true,
-    header: true,
-    dynamicTyping: true,
-    transformHeader: function(name) {
-        return name.toLowerCase().replace(" ","_").replace("%","pp")
-    },
-    transform: function(value, header) {
-        if (header == "r3_time" && value) {
-            let time = Date.parse(value.replace(/\s/,"T")+'Z') - 3600000;
-            if (time > Date.parse('2023-03-26T01:00:00Z')) {
-                time -= 3600000;
-            }
-            return time;
-        }
-        return value;
-    },
-    step: function(results) {
-        let obj = { rounds: [] };
-        for (let key in results.data) {
-            const found = key.match(/^r(\d)_(.*)$/);
-            if (found) {
-                const round = parseInt(found[1])-1;
-                const roundKey = found[2];
-                obj.rounds[round] ||= {}
-                obj.rounds[round][roundKey] = results.data[key];
-            } else {
-                obj[key] = results.data[key];
-            }
-        }
-        if (obj.rounds[0]) {
-            data.push(obj);
-        }
-    },
-    complete: function() {
-        const dataDom = document.getElementById("winner_table_body");
+function getWinLoseClass(pos) {
+    if (pos == 1) return 'win';
+    if (pos == -1) return 'lose';
+    return '';
+}
 
-        for (let thread of data) {
+function postDownload() {
+    const dataDom = document.getElementById("winner_table_body");
+    const tournDom = document.getElementById("round_table_body");
+
+    for (let positions = activePos; positions>=1; positions--) {
+        let latest = positions == activePos;
+
+        for (let thread of data[positions]) {
             thread.score = thread.rounds[0].points + thread.rounds[0].diff * 0.1;
             if (thread.rounds[1].points) {
                 thread.score += thread.rounds[1].points + thread.rounds[1].diff * 0.1;
@@ -377,7 +388,7 @@ Papa.parse('standings/1.csv', {
             if (thread.score < 0) thread.score = 0;
         }
 
-        let scores = data.filter(item => item.rounds[2].time).sort((a,b) => a.name > b.name ? 1 : -1);
+        let scores = data[positions].filter(item => item.rounds[2].time).sort((a,b) => a.name > b.name ? 1 : -1);
 
         let totalPoints = scores.reduce((sum,thread) => sum + thread.score, 0);
 
@@ -397,15 +408,31 @@ Papa.parse('standings/1.csv', {
                 if (oddsWin < 1) oddsWin = 1.01;
 
                 thread.odds['W'][pos] = parseFloat(oddsWin.toFixed(2));
-                result += `<td id="W_${thread.id}_${pos}">${oddsWin.toFixed(2)}</td>`;
+
+                if (latest) {
+                    scoreData['W'] ||= {}
+                    scoreData['W'][pos] ||= {}
+
+                    if (pos == 16) {
+                        if (thread.rounds[2].grp == 'X') {
+                            scoreData['W'][pos][thread.id] = -1;
+                        } else if (thread.rounds[2].grp == 'A') {
+                            scoreData['W'][pos][thread.id] = 1;
+                        }
+                    }
+
+                    result += `<td class="${getWinLoseClass(scoreData['W'][pos][thread.id])}" id="W_${thread.id}_${pos}">${oddsWin.toFixed(2)}</td>`;
+                }
             }
 
-            result += '</tr>';
-            dataDom.innerHTML += result;
+            if (latest) {
+                result += '</tr>';
+                dataDom.innerHTML += result;
+            }
         }
 
-        const tournDom = document.getElementById("round_table_body");
-        let tournaments = data.filter(item => item.rounds[2].pos == 1).sort((a,b) => a.rounds[2].time-b.rounds[2].time);
+
+        let tournaments = data[positions].filter(item => item.rounds[2].pos == 1).sort((a,b) => a.rounds[2].time-b.rounds[2].time);
 
         for (let thread of tournaments) {
             let klass = '';
@@ -415,7 +442,7 @@ Papa.parse('standings/1.csv', {
 
             let result = `<tr ${klass}><td>${thread.name}</td>`;
             let score_a = thread.score;
-            let thread_b = data.find(item => item.id == thread.rounds[2].enid);
+            let thread_b = data[positions].find(item => item.id == thread.rounds[2].enid);
             let score_b = thread_b.score;
             let total = score_a + score_b;
 
@@ -428,59 +455,136 @@ Papa.parse('standings/1.csv', {
 
                 thread.odds['A'][pos] = parseFloat(oddsWin.toFixed(2));
 
-                result += `<td id="A_${thread.id}_${pos}">${oddsWin.toFixed(2)}</td>`;
+                if (latest) {
+                    scoreData['A'] ||= {}
+                    scoreData['A'][pos] ||= {}
+                    if (thread.rounds[2].grp == 'A') {
+                        if (parseFloat(thread.rounds[2].pp) - 50 > pos/2) {
+                            scoreData['A'][pos][thread.id] = 1
+                        } else {
+                            scoreData['A'][pos][thread.id] = -1
+                        }
+                    } else if (thread.rounds[2].points > 0) {
+                        scoreData['A'][pos][thread.id] = -1
+                    }
+                    result += `<td class="${getWinLoseClass(scoreData['A'][pos][thread.id])}" id="A_${thread.id}_${pos}">${oddsWin.toFixed(2)}</td>`;
+                }
             }
             for (let pos of [0,10,25,33,50]) {
                 let oddsWin = 1 + Math.abs((total - score_b + score_a * (pos/100)) / (score_b - score_a * (pos/100)) / 2);
 
                 thread.odds['B'][pos] = parseFloat(oddsWin.toFixed(2));
 
-                result += `<td id="B_${thread.id}_${pos}">${oddsWin.toFixed(2)}</td>`;
+                if (latest) {
+                    scoreData['B'] ||= {}
+                    scoreData['B'][pos] ||= {}
+                    if (thread_b.rounds[2].grp == 'A') {
+                        if (parseFloat(thread_b.rounds[2].pp) - 50 > pos/2) {
+                            scoreData['B'][pos][thread.id] = 1
+                        } else {
+                            scoreData['B'][pos][thread.id] = -1
+                        }
+                    } else if (thread_b.rounds[2].points > 0) {
+                        scoreData['B'][pos][thread.id] = -1
+                    }
+
+                    result += `<td class="${getWinLoseClass(scoreData['B'][pos][thread.id])}" id="B_${thread.id}_${pos}">${oddsWin.toFixed(2)}</td>`;
+                }
             }
 
-            result += `<td>${thread.rounds[2].en}</td></tr>`;
-            tournDom.innerHTML += result;
+            if (latest) {
+                result += `<td>${thread.rounds[2].en}</td></tr>`;
+                tournDom.innerHTML += result;
+            }
         }
 
-        validity = scores.reduce((min, item) => item.rounds[2].time > now ? (min > item.rounds[2].time ? item.rounds[2].time : min)  : min, scores[0].rounds[2].time);
+        if (latest) {
+            validity = scores.reduce((min, item) => item.rounds[2].time > now ? (min > item.rounds[2].time ? item.rounds[2].time : min) : min, scores[0].rounds[2].time);
+            document.getElementById("valid_time").innerHTML = `Oddsok érvényesek: ${new Date(validity).toLocaleString('hu-HU', { timeZone: 'Europe/Budapest' } )}`;
+        }
+    }
 
-        document.getElementById("valid_time").innerHTML = `Oddsok érvényesek: ${new Date(validity).toLocaleString('hu-HU', { timeZone: 'Europe/Budapest' } )}`;
+    updateMoney();
+    updateCurrent();
+    updateBasket();
 
-        updateMoney();
-        updateCurrent();
-        updateBasket();
+    for (let elem of document.getElementsByTagName("td")) {
+        if (elem.id.startsWith("W_") || elem.id.startsWith("A_") || elem.id.startsWith("B_")) {
+            let elemIdPre = elem.id.split("_").splice(0,2).join("_") + "_";
+            let elemIdPre2 = elemIdPre.replace('A','X').replace('B','Y').replace('X','B').replace('Y','A');
 
-        for (let elem of document.getElementsByTagName("td")) {
-            if (elem.id.startsWith("W_") || elem.id.startsWith("A_") || elem.id.startsWith("B_")) {
-                let elemIdPre = elem.id.split("_").splice(0,2).join("_") + "_";
-                let elemIdPre2 = elemIdPre.replace('A','X').replace('B','Y').replace('X','B').replace('Y','A');
+            let elemIdPost = parseInt(elem.id.split("_")[2]);
 
-                let elemIdPost = parseInt(elem.id.split("_")[2]);
-
-                let currIdPre = elemIdPre.replaceAll("_","");
-                let currIdPre2 = elemIdPre2.replaceAll("_","");
-                elem.onclick = function() {
-                    if (!elem.parentElement.classList.contains("disabled")) {
-                        if (elem.classList.contains("selected")) {
-                            elem.classList.remove("selected");
-                            delete current[currIdPre];
-                        } else {
-                            for (let elem2 of document.getElementsByTagName("td")) {
-                                if (elem2.id.startsWith(elemIdPre) || elem2.id.startsWith(elemIdPre2)) {
-                                    elem2.classList.remove("selected");
-                                }
+            let currIdPre = elemIdPre.replaceAll("_","");
+            let currIdPre2 = elemIdPre2.replaceAll("_","");
+            elem.onclick = function() {
+                if (!elem.parentElement.classList.contains("disabled")) {
+                    if (elem.classList.contains("selected")) {
+                        elem.classList.remove("selected");
+                        delete current[currIdPre];
+                    } else {
+                        for (let elem2 of document.getElementsByTagName("td")) {
+                            if (elem2.id.startsWith(elemIdPre) || elem2.id.startsWith(elemIdPre2)) {
+                                elem2.classList.remove("selected");
                             }
-                            delete current[currIdPre];
-                            delete current[currIdPre2];
-
-                            current[currIdPre] = elemIdPost;
-
-                            elem.classList.add("selected");
                         }
-                        updateCurrent();
+                        delete current[currIdPre];
+                        delete current[currIdPre2];
+
+                        current[currIdPre] = elemIdPost;
+
+                        elem.classList.add("selected");
                     }
+                    updateCurrent();
                 }
             }
         }
     }
-});
+}
+
+let downloaded = 0;
+for (let positions = activePos; positions>=1; positions--) {
+    let latest = positions == activePos;
+    Papa.parse(`standings/${positions}.csv`, {
+        download: true,
+        header: true,
+        dynamicTyping: true,
+        transformHeader: function(name) {
+            return name.toLowerCase().replace(" ","_").replace("%","pp")
+        },
+        transform: function(value, header) {
+            if (header == "r3_time" && value) {
+                let time = Date.parse(value.replace(/\s/,"T")+'Z') - 3600000;
+                if (time > Date.parse('2023-03-26T01:00:00Z')) {
+                    time -= 3600000;
+                }
+                return time;
+            }
+            return value;
+        },
+        step: function(results) {
+            let obj = { rounds: [] };
+            for (let key in results.data) {
+                const found = key.match(/^r(\d)_(.*)$/);
+                if (found) {
+                    const round = parseInt(found[1])-1;
+                    const roundKey = found[2];
+                    obj.rounds[round] ||= {}
+                    obj.rounds[round][roundKey] = results.data[key];
+                } else {
+                    obj[key] = results.data[key];
+                }
+            }
+            if (obj.rounds[0]) {
+                data[positions] ||= [];
+                data[positions].push(obj);
+            }
+        },
+        complete: function() {
+            downloaded += 1;
+            if (downloaded == activePos) {
+                postDownload();
+            }
+        }
+    });
+}
